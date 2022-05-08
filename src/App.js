b0087cb638300f43d3179041620babba41a8e16e
2021MT93515 <103430348+2021MT93515@users.noreply.github.com>
2022-05-08 17:42:31 +0530

    Update App.js
    Added login code

--- src/App.js
+++ src/App.js
@@ -18,6 +18,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required />
+        
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required />
+            
+        <button type="submit" onClick={()=>{ alert('Login Successfully Done !'); }}>Login</button>
+      </div>
     </div>
   );
 }
