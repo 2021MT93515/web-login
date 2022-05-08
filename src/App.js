4c999dbb7bb699814091933ad33934ab163086c8
2021MT93515 <103430348+2021MT93515@users.noreply.github.com>
2022-05-08 18:02:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        Login
       </header>
       <div>
         <label for="uname"><b>Username</b></label>
